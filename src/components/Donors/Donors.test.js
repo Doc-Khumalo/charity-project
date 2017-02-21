9fa6a3897907d352a12704ed227c3c2859a10f14
Leslie <leslie.khumalo@icloud.com>
2017-02-21 08:42:03 +0000

    updated test

--- src/components/Donors/Donors.test.js
+++ src/components/Donors/Donors.test.js
@@ -6,7 +6,7 @@ import request from 'request';
 import { mount } from 'enzyme';
 import Donors from './Donors';
 
-var baseUrl = 'https://api.justgiving.com/cbbcb882/v1/charity/2116/donations;';
+var baseUrl = 'https://api.justgiving.com/cbbcb882/v1/charity/2116/donations';
 
 describe('Donors', () => {
     it('should render when all is ok', () => {
@@ -15,15 +15,15 @@ describe('Donors', () => {
 
     it('calls componentDidMount', (done) => {
         sinon.spy(Donors.prototype, 'componentDidMount');
-        const wrapper = mount(<Donors />);
+        mount(<Donors />);
         expect(Donors.prototype.componentDidMount.calledOnce).to.equal(true);
         done();
     });
 
     it('returns donors', () => {
-       request.get({ url: baseUrl }),
-         function (error, reponse, body) {
-             expect(response.statusCode).to.equal(200);
-         }
+        request.get({ url: baseUrl }),
+          function () {
+              expect(response.statusCode).to.equal(200);
+          }
     });
 });
